ffd5a4252e24d6dfed098886044a0c4abe9e1f47
Oleg Gaevoy <oleg.gaevoy@advarra.com>
2023-03-08 19:08:47 +0100

    basics

---
+++ 2_tests_fixture.py
@@ -0,0 +1,54 @@
+import re
+from playwright.sync_api import Page, expect
+import pytest
+import playwright
+
+#pip install pytest-xdist
+#run: pytest .\1_GetStarted.py --headed --numprocesses auto
+
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args, playwright):
+    iphone_11 = playwright.devices['iPhone 11 Pro']
+    return {
+        **browser_context_args,
+        **iphone_11,
+    }
+
+def test_homepage(page: Page, session):
+    page.goto("https://playwright.dev/")
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Playwright"))
+
+    # create a locator
+    get_started = page.get_by_role("link", name="Get started")
+
+    # Expect an attribute "to be strictly equal" to the value.
+    expect(get_started).to_have_attribute("href", "/docs/intro")
+
+    # Click the get started link.
+    get_started.click()
+
+    # Expects the URL to contain intro.
+    expect(page).to_have_url(re.compile(".*intro"))
+
+def test_homepage_2(page: Page, session):
+    page.goto("https://playwright.dev/")
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Playwright"))
+
+    # create a locator
+    get_started = page.get_by_role("link", name="Get started")
+
+    # Expect an attribute "to be strictly equal" to the value.
+    expect(get_started).to_have_attribute("href", "/docs/intro")
+
+    # Click the get started link.
+    get_started.click()
+
+    # Expects the URL to contain intro.
+    expect(page).to_have_url(re.compile(".*intro"))
+
+
+

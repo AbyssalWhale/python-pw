881c61e5014ba91fd6adf2b7cee59776e74dd9c8
OH <olegenikus@gmail.com>
2023-03-14 20:33:45 +0100

    locators update

--- 2_tests_fixture.py
+++ 2_tests_fixture.py
@@ -15,7 +15,12 @@ from Models.POM.HomePage import HomePage
 #     }
 
 def test_homepage(playwright: Playwright):
+    #Arrange
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
     home_Page = HomePage(page)
+
+    #Assert
     expect(home_Page.p_Page).to_have_title(re.compile("Main Page"))
+    expect(home_Page.label_title).to_be_visible()
+    expect(home_Page.label_titleDescription).to_be_visible()

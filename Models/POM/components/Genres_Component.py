281722ba230739094013ba7a9d2f349ad7e7b608
AbyssalWhale <53709071+AbyssalWhale@users.noreply.github.com>
2023-05-17 12:08:25 +0200

    test_game_category_can_be_selected improvements and additional scenario

--- Models/POM/components/Genres_Component.py
+++ Models/POM/components/Genres_Component.py
@@ -6,5 +6,8 @@ class Genres_Component(ComponentBase):
         super().__init__(playwrightPage, componentTitle)
 
     def click_genre_button(self, genreName: str):
-        self.p_Page.locator(f"//ul//button[text()='{genreName}']").click()
-        return self
+        with self.p_Page.expect_response(f"**/api/games**&genres=**") as response_info:
+            self.p_Page.locator(f"//ul//button[text()='{genreName}']").click()
+        assert response_info.value.ok
+        genre_games = response_info.value.json()
+        return genre_games
